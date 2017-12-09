68769f64b2fbc840be837b43d1dabc909a5dd2d8
Amy <aschultz@invoca.com>
2017-12-09 13:18:20 -0800

    Update color scheme, incorporate issue #2

--- app/src/components/Chore.js
+++ app/src/components/Chore.js
@@ -11,34 +11,11 @@ class Chore extends React.Component {
     users: PropTypes.array
   }
 
-  // callApi(){
-  //   fetch("http://localhost:5000/chores/" + this.props.id)
-  //     .then((response) => response.json())
-  //     .then((responseJson) => {
-  //       this.setState({ chores: responseJson });
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     })
-  // }
-
-  // componentWillMount(){
-  //   this.callApi();
-  //
-  //   this.interval = setInterval(() => {
-  //     this.callApi();
-  //   }, 5000);
-  // }
-  //
-  // componentWillUnmount(){
-  //   clearInterval(this.interval);
-  // }
-
   // on click, rotate chore
   render(){
     const content = this.props.users.map((user, index) =>
       <div key={user.id + "user"}>
-        <User name={user.name} index={index} choreId={this.props.id} id={user.id} />
+        <User name={user.name} index={index} choreId={this.props.id} id={user.id} firstUser={index === 0}/>
       </div>
     )
 
--- app/src/components/User.js
+++ app/src/components/User.js
@@ -8,7 +8,8 @@ class User extends React.Component {
     super(props);
 
     this.state = {
-      selectedComponent: false
+      selectedComponent: false,
+      firstUser: false
     };
   }
 
@@ -19,36 +20,45 @@ class User extends React.Component {
     choreId: PropTypes.number
   }
 
+  confirmationDialog(){
+    return (<div className="row">
+      <div onClick={() => {this.handleConfirmationClick()}} className="col-6 confirmation-text">
+        CONFIRM
+      </div>
+      <div className="col-6 confirmation-text">
+        CANCEL
+      </div>
+    </div>);
+  }
+
   displayText(){
     if(this.state.selectedComponent === true) {
-      return <span className="user-text">Are you sure?</span>
+      return this.confirmationDialog();
     } else {
       return <span className="user-text">{this.props.name}</span>
     }
   }
 
-  handleClick(){
+  handleConfirmationClick(){
     if (this.state.selectedComponent === true) {
       var userJson = JSON.stringify({ 'user_id': this.props.id, 'id': this.props.choreId });
       fetch(`https://choreapi.herokuapp.com/chores/${this.props.choreId}/rotate.json`, { method: 'post',
                                               headers: { "Content-Type": "application/json"},
                                               body: userJson })
     }
+  }
 
+  handleUserClick(){
     this.toggleState();
   }
 
-  toggleState(){
-    if(this.state.selectedComponent === true) {
-      this.setState({selectedComponent: false})
-    } else {
-      this.setState({selectedComponent: true})
-    }
+  toggleState() {
+    this.setState({ selectedComponent: !this.state.selectedComponent });
   }
 
   render(){
     return(
-      <div onClick={() => {this.handleClick()}} className={`card user-box user-${this.props.index}`}>
+      <div onClick={() => {this.handleUserClick()}} className={`card user-box user-${this.props.index}`}>
         {this.displayText()}
       </div>
     )
